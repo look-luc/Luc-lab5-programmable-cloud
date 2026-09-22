deb70b91ef249168b382ec0c3bc4ae0f96b17b1d
look-luc <denardiluc@gmail.com>
2026-09-21 18:46:00 -0600

    fixing issues

--- part1/setup.sh
+++ part1/setup.sh
@@ -3,6 +3,7 @@
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip git
 sudo pip3 install google-cloud-compute
+
 git clone https://github.com/cu-csci-4253-datacenter/flask-tutorial
 cd flask-tutorial
 sudo python3 setup.py install
