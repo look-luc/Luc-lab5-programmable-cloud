ead15487781e48ac0df50f67fcb74c769b453cb9
look-luc <denardiluc@gmail.com>
2026-09-23 13:29:12 -0600

    Update vm_app.py

--- part1/vm_app.py
+++ part1/vm_app.py
@@ -68,7 +68,7 @@ def create_instance(
         operation.result()
         print("Firewall rule successfully created!")
 
-    with open("setup.sh", "r") as file:
+    with open("./part1/setup.sh", "r") as file:
         bash_file = file.read()
 
     metadata = compute_v1.Metadata(
